28d572b974b32bb26a2498a7d9058d24884a59c4
Rudraksh Singh Bhadauria <117443595+rudraksh2611@users.noreply.github.com>
2023-05-18 14:39:06 +0530

    Add files via upload

---
+++ lecture3.cpp
@@ -0,0 +1,37 @@
+// LECTURE-3
+// Printing Number Of Apples:
+/*#include<iostream>
+using namespace std;
+int main()
+{
+    int apples;
+    cin>>apples;
+    cout<<"Number of apples: "<<apples<<"\n";
+ return 0;   
+}*/
+
+
+// SWAPPING 2 NUMBERS 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"a:";
+    cin>>a;
+    cout<<"b:";
+    cin>>b;
+
+    int c;
+    c=b;
+    b=a;
+    a=c;
+
+    cout<<"a:"<<a;
+    cout<<"\nb:"<<b;
+return 0;
+} 
+
+//--------------x----------------
+
+
