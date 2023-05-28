a6c5142e3885deff0307c651d373a5a2f4687c38
Rudraksh Singh Bhadauria <117443595+rudraksh2611@users.noreply.github.com>
2023-05-28 13:49:01 +0530

    Questions on Loops
    Added Question from [3 to 6].
    Updated on - 28/05/2023

--- lecture8.cpp
+++ lecture8.cpp
@@ -42,7 +42,7 @@ int main()
 */
 
 //Question 3-> Reverse the digit number:
-#include<bits/stdc++.h>
+/*#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
@@ -58,5 +58,69 @@ int main()
         n/=10;
     }
     cout<<"Reverse Number is:"<<reverse<<endl;
+    return 0;
+}*/
+
+// Question 4-> Given 2 numbers 'a' and 'b'. Find 'a' raise to the power 'b':
+/*#include<bits./stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter A which is your base number:";
+    cin>>a;
+    cout<<"Enter B which is your base to the 'Power' number:";
+    cin>>b;
+
+    int result=1;
+    for (int i=1; i<=b; i++)
+    {
+        result*=a;
+    }
+    cout<<"Your Answer is:"<<result<<endl;
+    return 0;
+    
+}*/
+
+//Question 5-> Find the sum of a Series -> (Series=1-2+3-4....n)
+/*#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number till which you want to get your series extended:";
+    cin>>n;
+
+    int result=0;
+    for(int i=1;i<=n;i++)
+    {
+        if(i%2==0)//If the number of terms is even
+        {
+            result -=i;
+        }
+        else
+        {
+            result +=i;
+        }
+    }
+    cout<<"Your Sum of the given series is:"<<result;
+    return 0;
+}*/
+
+//Question 6->Print the first 'n' Factorial:
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter a nunber of what you want a factorial:";
+    cin>>n;
+
+    int factorial =1;
+    for (int i=1;i<=n;i++)
+    {
+        factorial *=i;
+    }
+    cout<<"Yoour answeer is:"<<factorial<<endl;
     return 0;
 }
