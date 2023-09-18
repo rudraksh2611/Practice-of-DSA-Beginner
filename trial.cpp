44a3848dc8aec6ecacf27605395ae0017eed0643
Rudraksh Singh Bhadauria <117443595+rudraksh2611@users.noreply.github.com>
2023-09-18 10:54:25 +0530

    Update trial.cpp
    i did think and try

--- trial.cpp
+++ trial.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+/*#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
@@ -6,4 +6,32 @@ int main()
  cin>>apples;
  cout<<"No. of Apples given by user:"<<apples;
  return 0;   
+}
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    // Write C++ code here
+    string wish;
+    string temperature;
+    cout << "Type Your morning wish:"<<endl;
+    cin>>wish;
+    if(wish=="gm"){
+        cout<<"Good morning sir/ma'am, Should i tell you about temperature if yes then type temperature below 😊:"<<endl;
+        cin>>temperature;
+        if(temperature=="temperature"){
+            cout<<"It's partially cloudy outside! Thank You 😊"<<endl;
+        }
+        else
+        {
+            cout<<"Your input not in our system,have a nice day ahead"<<endl;
+        }
+    }
+    else 
+    {
+        cout<<"Your input not in our system,have a nice day ahead!"<<endl;
+    }
+
+    return 0;
 }
